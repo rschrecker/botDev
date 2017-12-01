9eca3103f3281a74921a91cb448c2d06114192ad
Rowan Schrecker <Rowan.Schrecker@softwire.com>
2017-12-01 11:59:12 +0000

    too many dynamite

--- forgetMeBot.js
+++ forgetMeBot.js
@@ -4,22 +4,25 @@ function rand (list) {
 
 function lastRoundWasDraw(gamestate) {
     const len = gamestate.rounds.length;
+    if (len === 0) {
+        return false
+    }
     return gamestate.rounds[len - 1][1] === gamestate.rounds[len - 1][2];
 }
 
 function countDynamite (gamestate, player) {
-    return gamestate.rounds.map(round => round.player).filter(move => move === 'D').length;
+    return gamestate.rounds.map(round => round.player).filter(move => (move === 'D')).length;
 }
 
 function dynamiteRatio (gamestate, player) {
-    return countDynamite(gamestate, player)/gamestate.rounds.map(round => round.player).length;
+    return countDynamite(gamestate, player)/gamestate.rounds.length;
 }
 
 const rps = ['R', 'P', 'S'];
 
 class ForgetMeBot {
     makeMove(gamestate) {
-        if (lastRoundWasDraw(gamestate) && dynamiteRatio(gamestate, 1) >= 0.09) {
+        if (lastRoundWasDraw(gamestate) && (dynamiteRatio(gamestate, 1) <= 0.05)) {
             return rand([rand(rps), 'W', 'D']);
         }
         return rand(rps);
